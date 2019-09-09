7a4bbec5926afbb5f30fe1de76a43ccedce538e2
Simon Prast <simon.prast.sp@gmail.com>
2019-09-09 14:34:58 +0200

    Change the order and icons of the menu items
    The order as well as the icons of the different menu tabs were not
    created thoughtfully. Some are unnecessary displayed and some others had
    the same menu icon.
    
    The icons of some menu items are changed now and Model Groups are added:
    The UserAdmin, AnamneseAdmin and BeautyreportAdmin classes are now
    grouped together in a ModelAdminGroup within the Beautyreport app's
    wagtail_hooks.py file.
    Also the Coach and the Customer menu labels are renamed to be plural.

--- charm/customer/wagtail_hooks.py
+++ charm/customer/wagtail_hooks.py
@@ -4,9 +4,9 @@ from .models import Customer
 
 class CustomerAdmin(ModelAdmin):
     model = Customer
-    menu_label = "Customer"
-    menu_icon = "user"
-    menu_order = 290
+    menu_label = "Customers"
+    menu_icon = "group"
+    menu_order = 110
     add_to_settings_menu = False
     exclude_from_explorer = False
    
