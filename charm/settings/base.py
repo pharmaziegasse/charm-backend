9f8e85be4826331e3d14e59d8b3e4aa15ec11b86
Simon Prast <simon.prast.sp@gmail.com>
2019-09-11 00:07:14 +0200

    Add Custom Theme for Wagtail
    A purple custom theme is added for the Wagtail CMS.
    This should fit the color theme of Charm. :)

--- charm/settings/base.py
+++ charm/settings/base.py
@@ -25,6 +25,7 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 INSTALLED_APPS = [
     # These are our custom written apps.
+    'charm.core',
     'charm.api',
     'charm.home',
     'charm.reports',
