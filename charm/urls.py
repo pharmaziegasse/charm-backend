f66e9bf8db5348bcb4e9ccd621bd8e3f480c7405
Simon Prast <simon.prast.sp@gmail.com>
2019-09-18 14:41:52 +0200

    Add root page redirect
    Redirect a request on the root page to /admin/.
    The root page should not be accessed in our setup.

--- charm/urls.py
+++ charm/urls.py
@@ -12,6 +12,8 @@ from wagtail.documents import urls as wagtaildocs_urls
 # graphene-django GraphQLView
 from graphene_django.views import GraphQLView
 
+from .redirect import redirect_view
+
 
 # from search import views as search_views
 
@@ -30,6 +32,7 @@ urlpatterns = [
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
+    url(r'^$', redirect_view),
     url(r'', include(wagtail_urls)),
 
     # Alternatively, if you want Wagtail pages to be served from a subpath
