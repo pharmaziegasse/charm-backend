69c4da1f089b272c683f82ee38674eaeecb8a4e0
Simon Prast <simon.prast.sp@gmail.com>
2019-09-18 15:01:36 +0200

    Remove body field from home page model
    The home page model does not need the body field.
    This was a placeholder and is removed now.

--- charm/home/models.py
+++ charm/home/models.py
@@ -5,12 +5,9 @@ from wagtail.admin.edit_handlers import FieldPanel, TabbedInterface, ObjectList
 
 # Custom Homepage Model to provide global data later on
 class CharmPage(Page):
-    body = RichTextField(null=True, blank=True, help_text="Site's body")
-    
     # These panels will be listed on the Wagtail admin site where you can edit the page.
     main_content_panels = [
         FieldPanel('title', classname="full title"),
-        FieldPanel('body', classname="full"),
     ]
 
     # By defining the edit_handler, the panels are finally displayed
